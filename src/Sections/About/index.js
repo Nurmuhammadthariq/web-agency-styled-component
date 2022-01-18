3809f67c08f36e0c5a0cf424e473835ae59ce43e
thoriqnoer <thoriq.noer@gmail.com>
2022-01-18 11:12:39 +0700

    Creating about section

--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import Wave from '../../assets/waves.svg';
+import hand from '../../assets/hand.svg';
+import rocket from '../../assets/rocket image.png';
+import human from '../../assets/human.svg';
+
+import {
+  AboutSection,
+  Waves,
+  Hand,
+  Main,
+  Title,
+  CurvedLine,
+  Content,
+  Rocket,
+  Human,
+  AboutText,
+  Text,
+  Circle,
+} from './AboutElements';
 
 const About = () => {
-  return <div>Section About</div>;
+  return (
+    <AboutSection id="about">
+      <Waves src={Wave} alt="" />
+      {/* <Hand>
+        <img src={hand} alt="" />
+      </Hand> */}
+      <Main>
+        <div>
+          <Title>About Us</Title>
+          <CurvedLine />
+        </div>
+        <Content>
+          <Rocket>
+            <img src={rocket} alt="" width="400" height="400" />
+          </Rocket>
+          <AboutText>
+            {/* <Human>
+              <img src={human} alt="" />
+            </Human> */}
+
+            <Text>
+              We help our customers to tell about themselves, to grow and stand
+              out in an increasingly competitive digital world, through creative
+              projects that are able to attract and involve, creating strategic
+              value.
+            </Text>
+            <div>
+              <Circle style={{ backgroundColor: 'var(--purple)' }} />
+              <Circle style={{ backgroundColor: 'var(--pink)' }} />
+              <Circle style={{ backgroundColor: 'var(--black)' }} />
+            </div>
+          </AboutText>
+        </Content>
+      </Main>
+    </AboutSection>
+  );
 };
 
 export default About;
